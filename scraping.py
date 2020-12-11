09c8f41fa5f598b56fee6d9a6438eb56aeb5b313
zoomdmartin02 <72090048+zoomdmartin02@users.noreply.github.com>
2020-12-11 11:41:18 -0600

    10.5.3

--- scraping.py
+++ scraping.py
@@ -3,6 +3,28 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
+
+# Initialize browser, create data dictionary and end the WebDriver and return the scraped data.
+def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+
+    # set news title and paragraph variables
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 # Set the executable path and initialize the chrome browser in splinter
 executable_path = {'executable_path': 'chromedriver'}
@@ -86,8 +108,9 @@ def mars_facts():
 
     return df.to_html()
 
-
-browser.quit()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
 
